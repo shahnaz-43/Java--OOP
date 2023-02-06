c1c632279a5659bd0baad207a6e45128675f189a
shahnaz-43 <kirei.kanan@gmail.com>
2023-02-05 22:40:52 -0500

    UPLOAD

---
+++ src/Method/Test1.java
@@ -0,0 +1,19 @@
+package Method;
+
+    public class Test1 {
+        public static void main(String[] args) {
+            Teacher t1 = new Teacher();
+            t1.name = "Moshiur";
+            t1.age = 26;
+            t1.gender="Male";
+            t1.qualification = "CSE";
+            t1.display();
+            Teacher t2 = new Teacher();
+            t2.name = "Rahman";
+            t2.age = 24;
+            t2.gender = "Male";
+            t2.qualification = "CSE";
+            t2.display();
+        }
+    }
+
