aa8c3d79868b4f54a081f3242572692bbedc8f80
shahnaz-43 <kirei.kanan@gmail.com>
2023-02-05 22:41:33 -0500

    Upload

--- src/Method/Test.java
+++ src/Method/Test.java
@@ -4,16 +4,16 @@ public class Test {
 
     public static void main(String[] args) {
         Teacher t1 = new Teacher();
-        t1.name = "Robert Jhon";
-        t1.age = 33;
+        t1.name = "Moshiur";
+        t1.age = 26;
         t1.gender="Male";
-        t1.qualification = "MSc in ML";
+        t1.qualification = "CSE";
         t1.display();
         Teacher t2 = new Teacher();
-        t2.name = "Suphia Kalberg";
-        t2.age = 30;
-        t2.gender = "Female";
-        t2.qualification = "MSc in CS";
+        t2.name = "Rahman";
+        t2.age = 24;
+        t2.gender = "Male";
+        t2.qualification = "CSE";
         t2.display();
     }
 }
